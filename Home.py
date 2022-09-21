71b7d69f78433aefb6aa539e53a8f6077caed83f
mathiasrostagno <109281847+mathiasrostagno@users.noreply.github.com>
2022-09-21 15:57:42 +0200

    up

--- Home.py
+++ Home.py
@@ -1,12 +1,14 @@
 import streamlit as st
 import streamlit.components.v1 as components
 from Constantes import *
+from PIL import Image
 
 
 st.set_page_config(page_title='Mathias Rostagno\'s portfolio' ,layout="wide",page_icon='bar_chart')
 
 st.subheader('Mathias Rostagno - Data Analyst')
 st.write(infosperso['Intro'])
+Certification = Image.open('data/Certification.png')
 st.image(Certification, caption='Professional Certification')
 
 with st.sidebar:
